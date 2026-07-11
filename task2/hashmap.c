5143c0fca666b577209f01ae73b85cf00f96d9b3
HassnainJavaid <bscs24041@itu.edu.pk.com>
2026-07-11 16:10:52 +0500

    HashMaps Under Implementation

---
+++ task2/hashmap.c
@@ -0,0 +1,14 @@
+#include "hashmap.h"
+#include <stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+
+static unsigned long hash_function(const char* str){
+    unsigned long hash = 1469598103934665603UL;
+
+    while(*str)
+    {
+        
+    }
+}
---
+++ task2/hashmap.h
@@ -0,0 +1,21 @@
+#ifndef HASH_MAP_H
+#define HASH_MAP_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef struct HashMap HashMap;
+typedef struct HashNode HashNode;
+
+HashMap* hash_map_create(size_t initial_Capacity);
+void hash_map_destroy(HashMap* map);
+void hash_map_insert(HashMap* map,const char* key,int val);
+bool hash_map_get(const HashMap* map,const char* key,int* out_valeu);
+bool hash_map_get_string(const Hashmap* map,int key ,const char** out_valeu);
+void hash_map_remove(const HashMap* map,const char*);
+size_t hash_map_size(const HashMap* map);
+void hash_map_clear(HashMap* map);
+bool hash_map_contains(const HashMap* map,const char* key);
+void hash_map_print(const HashMap* map);
+
+#endif
